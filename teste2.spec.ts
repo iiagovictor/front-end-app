f1784b81da89e5c86dea817b0969410ac292a4c0
Iago Victor <55510607+iiagovictor@users.noreply.github.com>
2023-06-27 17:55:52 -0300

    Create teste2.spec.ts

---
+++ teste2.spec.ts
@@ -0,0 +1,82 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { JobsComponent } from './jobs.component';
+import { JobsService } from './jobs.service';
+import { of } from 'rxjs';
+
+describe('JobsComponent', () => {
+  let component: JobsComponent;
+  let fixture: ComponentFixture<JobsComponent>;
+  let jobsService: JobsService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [JobsComponent],
+      imports: [HttpClientTestingModule],
+      providers: [JobsService]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JobsComponent);
+    component = fixture.componentInstance;
+    jobsService = TestBed.inject(JobsService);
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should fetch jobs on component initialization', () => {
+    const mockResponse = {
+      jobs: [
+        {
+          "1": "",
+          "4": "",
+          "name": "",
+          "details": ""
+        }
+      ],
+      tables: [
+        {
+          "1": "",
+          "4": "",
+          "name": "",
+          "location_path": "",
+          "layer": "",
+          "trans_operations": "",
+          "database": "",
+          "schema": ""
+        }
+      ],
+      edges: [
+        {
+          "from": "",
+          "to": ""
+        }
+      ]
+    };
+
+    spyOn(jobsService, 'getJobs').and.returnValue(of(mockResponse));
+
+    fixture.detectChanges();
+
+    expect(component.jobs).toEqual(mockResponse.jobs);
+    expect(component.graphData).toBeDefined();
+    expect(component.errorMsg).toBeUndefined();
+    expect(component.isLoading).toBe(true);
+  });
+
+  it('should handle error when fetching jobs', () => {
+    const errorMessage = 'Erro ao recuperar Jobs';
+
+    spyOn(jobsService, 'getJobs').and.returnValue(Promise.reject(new Error(errorMessage)));
+
+    fixture.detectChanges();
+
+    expect(component.jobs).toEqual([]);
+    expect(component.graphData).toBeUndefined();
+    expect(component.errorMsg).toEqual(`Erro ao recuperar Jobs = ${errorMessage}`);
+    expect(component.isLoading).toBe(true);
+  });
+});
