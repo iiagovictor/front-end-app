81b43cffea81130fda9706ad1bbd409de56e33e7
Iago Victor <55510607+iiagovictor@users.noreply.github.com>
2023-06-27 17:04:55 -0300

    Update teste.spec.ts

--- teste.spec.ts
+++ teste.spec.ts
@@ -1,16 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { JobsComponent } from './jobs.component';
 import { JobsService } from './jobs.service';
-import { of, throwError } from 'rxjs';
+import { createGraphData } from '../shared/utils/jobs';
 
 describe('JobsComponent', () => {
   let component: JobsComponent;
   let fixture: ComponentFixture<JobsComponent>;
-  let jobsService: JobsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [JobsComponent],
+      imports: [HttpClientModule],
       providers: [JobsService]
     }).compileComponents();
   });
@@ -18,38 +19,80 @@ describe('JobsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(JobsComponent);
     component = fixture.componentInstance;
-    jobsService = TestBed.inject(JobsService);
-    spyOn(jobsService, 'getJobs').and.returnValue(of({ jobs: [] }));
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should fetch jobs and update properties', async () => {
-    const jobs = [{ id: 1, title: 'Job 1' }, { id: 2, title: 'Job 2' }];
-    spyOn(jobsService, 'getJobs').and.returnValue(of({ jobs }));
-    spyOn(component, 'createGraphData');
+  it('should fetch jobs and populate the component properties', async () => {
+    const jobsService = TestBed.inject(JobsService);
+    const jobsResponse = {
+      jobs: [
+        {
+          '1': '',
+          '4': '',
+          name: 'Job 1',
+          details: 'Job details'
+        }
+      ],
+      tables: [
+        {
+          '1': '',
+          '4': '',
+          name: 'Table 1',
+          location_path: '',
+          layer: '',
+          trans_operations: '',
+          database: '',
+          schema: ''
+        }
+      ],
+      edges: [
+        {
+          from: '',
+          to: ''
+        }
+      ]
+    };
+    spyOn(jobsService, 'getJobs').and.returnValue(Promise.resolve(jobsResponse));
+    spyOn(component, 'fetchJob').and.callThrough();
 
-    await component.fetchJob();
-
-    expect(jobsService.getJobs).toHaveBeenCalled();
-    expect(component.jobs).toEqual(jobs);
-    expect(component.createGraphData).toHaveBeenCalledWith({ jobs });
+    expect(component.isLoading).toBe(false);
+    expect(component.jobs.length).toBe(0);
+    expect(component.graphData).toBeUndefined();
     expect(component.errorMsg).toBeUndefined();
+
+    await component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(component.fetchJob).toHaveBeenCalled();
     expect(component.isLoading).toBe(true);
+    expect(component.jobs.length).toBe(1);
+    expect(component.jobs[0].name).toBe('Job 1');
+    expect(component.graphData).toEqual(createGraphData(jobsResponse));
+    expect(component.errorMsg).toBeUndefined();
   });
 
-  it('should handle error during job fetch', async () => {
+  it('should handle error while fetching jobs', async () => {
+    const jobsService = TestBed.inject(JobsService);
     const errorMessage = 'Error fetching jobs';
-    spyOn(jobsService, 'getJobs').and.returnValue(throwError(new Error(errorMessage)));
+    spyOn(jobsService, 'getJobs').and.returnValue(Promise.reject(new Error(errorMessage)));
+    spyOn(component, 'fetchJob').and.callThrough();
 
-    await component.fetchJob();
+    expect(component.isLoading).toBe(false);
+    expect(component.jobs.length).toBe(0);
+    expect(component.graphData).toBeUndefined();
+    expect(component.errorMsg).toBeUndefined();
 
-    expect(jobsService.getJobs).toHaveBeenCalled();
-    expect(component.jobs).toEqual([]);
-    expect(component.createGraphData).not.toHaveBeenCalled();
-    expect(component.errorMsg).toBe(`Erro ao recuperar Jobs = ${errorMessage}`);
+    await component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(component.fetchJob).toHaveBeenCalled();
     expect(component.isLoading).toBe(true);
+    expect(component.jobs.length).toBe(0);
+    expect(component.graphData).toBeUndefined();
+    expect(component.errorMsg).toBe(`Erro ao recuperar Jobs = ${errorMessage}`);
   });
 });
