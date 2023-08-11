31f0cf864ae81e22ad8e14335e80a4a3d6fd6167
Iago Victor <55510607+iiagovictor@users.noreply.github.com>
2023-08-11 18:19:57 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,5 @@
+import re
+
 def validate_key(key):
     key = key.lower() # Converte a chave para minúsculas
 
@@ -14,12 +16,27 @@ def validate_key(key):
         return False, "RACF inválida"
 
     # Verifica o nome do projeto
-    match = re.match(r'^(run|temp|proj)_[a-z]{2}\d{4}_(?:[a-z]{7}_)?([a-zA-Z0-9]+)$', key)
+    match = re.match(r'^(run|temp|proj)_[a-z]{2}\d{4}_(?:[a-z]{7}_)?(\S+)$', key)
     if match:
         project_name = match.group(2)
-        if len(project_name) > 1:
+        if len(project_name) >= 1:
             return True, ""
         else:
-            return False, "Nome do recurso deve ter 2 caracteres no mínimo"
+            return False, "Nome do recurso deve ter 1 caracter no mínimo"
     else:
         return False, "Formato inválido"
+
+# Exemplos de uso
+keys = [
+    "run_ab1234_project1",
+    "temp_cd5678_VICTIAG_project2",
+    "proj_ef9012_victiag_project3_special-char!",
+    "invalid_key"
+]
+
+for key in keys:
+    valid, reason = validate_key(key)
+    if valid:
+        print(f"{key} é válido")
+    else:
+        print(f"{key} é inválido, {reason}")
