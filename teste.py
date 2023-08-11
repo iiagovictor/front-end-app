60941c5ee6549fd20f5a36801c8e8bc98127d267
Iago Victor <55510607+iiagovictor@users.noreply.github.com>
2023-08-11 11:19:12 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -11,15 +11,17 @@ for state_machine in state_machines['stateMachines']:
     state_machine_arn = state_machine['stateMachineArn']
     print(f"State Machine ARN: {state_machine_arn}")
 
-    # Liste as execuções para esta state machine
-    executions = sfn_client.list_executions(
-        stateMachineArn=state_machine_arn,
-        statusFilter='RUNNING'
+    # Liste todas as execuções para esta state machine
+    all_executions = sfn_client.list_executions(
+        stateMachineArn=state_machine_arn
     )
 
+    total_executions = len(all_executions['executions'])
+    print(f"Total de Execuções: {total_executions}")
+
     # Se houver execuções, obtenha a mais recente
-    if executions['executions']:
-        last_execution = executions['executions'][0]
+    if all_executions['executions']:
+        last_execution = all_executions['executions'][0]
         last_execution_date = last_execution['startDate']
         print(f"Última Execução: {last_execution['executionArn']} - Data: {last_execution_date} - Status: {last_execution['status']}")
     else:
