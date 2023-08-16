6221be25f90af494b5028e9741023ce2caca9883
Iago Victor <55510607+iiagovictor@users.noreply.github.com>
2023-08-16 15:35:54 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,12 +1,46 @@
 import boto3
-import sys
 
-# Baixar common_functions.py do S3
-s3_resource = boto3.resource('s3')
-s3_resource.Bucket('your-bucket-name').download_file('path/to/common_functions.py', '/tmp/common_functions.py')
+def run_athena_query(query, database, s3_output):
+    """
+    Executa uma consulta no Athena e retorna o resultado.
+    
+    Parâmetros:
+    - query: A consulta SQL que você deseja executar.
+    - database: O nome do banco de dados Athena.
+    - s3_output: O local do S3 onde os resultados da consulta serão salvos.
+    """
+    client = boto3.client('athena')
+    
+    response = client.start_query_execution(
+        QueryString=query,
+        QueryExecutionContext={
+            'Database': database
+        },
+        ResultConfiguration={
+            'OutputLocation': s3_output,
+        }
+    )
+    
+    query_execution_id = response['QueryExecutionId']
+    
+    # Aguarde a consulta ser concluída e obtenha o status
+    status = 'RUNNING'
+    while status in ['RUNNING', 'QUEUED']:
+        response = client.get_query_execution(QueryExecutionId=query_execution_id)
+        status = response['QueryExecution']['Status']['State']
+    
+    if status == 'SUCCEEDED':
+        print("Consulta concluída com sucesso.")
+    elif status == 'FAILED':
+        print("Consulta falhou.")
+    else:
+        print("Consulta foi cancelada.")
+    
+    return status
 
-# Importando funções do arquivo common_functions.py
-sys.path.append('/tmp')
-import common_functions as cf
-
-# Continuação do seu código...
+# Exemplo de uso:
+query = "SELECT * FROM minha_tabela LIMIT 10;"
+database = "meu_banco_de_dados"
+s3_output = "s3://meu-bucket/caminho/para/saida/"
+run_athena_query(query, database, s3_output)
+``
