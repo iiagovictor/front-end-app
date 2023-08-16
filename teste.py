1be8c8af54e848101d38155f5bc6b873e24f8aba
Iago Victor <55510607+iiagovictor@users.noreply.github.com>
2023-08-16 16:58:05 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,14 +1,1 @@
-# ... [restante do código acima]
-
-    # Convertendo os dados em uma lista de dicionários e removendo aspas extras
-    lines = data.split("\n")
-    results = [{line.split(",")[1].strip('"'): line.split(",")[0].strip('"')} for line in lines[1:] if line]  # Ignorando o cabeçalho e linhas vazias
-
-# ... [restante do código abaixo]
-
-# Exemplo de lista
-lista = [{"uma": "valor1"}, {"outra": "valor2"}, {"uma": "valor3"}]
-
-for l in lista:
-    if next(iter(l)) == 'uma':
-        print(l[next(iter(l))])
+', '.join([f"'{item}'" for item in lista])
