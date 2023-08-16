3a6b8d30cc8c93120fe78c71eaf12e956b680e8f
Iago Victor <55510607+iiagovictor@users.noreply.github.com>
2023-08-16 15:57:38 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,9 +1,9 @@
 import boto3
-import pandas as pd
+import time
 
-def run_athena_query_and_get_results(query, database, s3_output):
+def run_athena_query_and_get_dict_list(query, database, s3_output):
     """
-    Executa uma consulta no Athena e retorna os resultados em um dataframe do pandas.
+    Executa uma consulta no Athena e retorna os resultados em uma lista de dicionários.
     
     Parâmetros:
     - query: A consulta SQL que você deseja executar.
@@ -27,27 +27,31 @@ def run_athena_query_and_get_results(query, database, s3_output):
     # Aguarde a consulta ser concluída e obtenha o status
     status = 'RUNNING'
     while status in ['RUNNING', 'QUEUED']:
+        time.sleep(5)  # Adiciona uma pausa de 5 segundos
         response = client.get_query_execution(QueryExecutionId=query_execution_id)
         status = response['QueryExecution']['Status']['State']
     
     if status != 'SUCCEEDED':
         print("Consulta não teve sucesso.")
-        return None
+        return None, 0
     
-    # Se a consulta foi bem-sucedida, leia os resultados do S3 em um dataframe do pandas
+    # Se a consulta foi bem-sucedida, leia os resultados do S3 em uma lista de dicionários
     result_s3_path = s3_output + query_execution_id + '.csv'
     s3 = boto3.resource('s3')
     bucket_name, key_name = result_s3_path.replace("s3://", "").split("/", 1)
     obj = s3.Object(bucket_name, key_name)
     data = obj.get()['Body'].read().decode('utf-8')
     
-    dataframe = pd.read_csv(pd.compat.StringIO(data))
+    # Convertendo os dados em uma lista de dicionários
+    lines = data.split("\n")
+    results = [{"servico": line.split(",")[1], "nome_recurso": line.split(",")[0]} for line in lines[1:] if line]  # Ignorando o cabeçalho e linhas vazias
     
-    return dataframe
+    return results, len(results)
 
 # Exemplo de uso:
-query = "SELECT * FROM minha_tabela LIMIT 10;"
+query = "SELECT nome_recurso, servico FROM minha_tabela LIMIT 10;"
 database = "meu_banco_de_dados"
 s3_output = "s3://meu-bucket/caminho/para/saida/"
-df = run_athena_query_and_get_results(query, database, s3_output)
-print(df)
+results_list, results_len = run_athena_query_and_get_dict_list(query, database, s3_output)
+print(results_list)
+print("Número de itens:", results_len)
