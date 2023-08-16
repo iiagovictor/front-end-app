9e3474e7a3bb8657cd1d32138f0292b19bbab05d
Iago Victor <55510607+iiagovictor@users.noreply.github.com>
2023-08-16 15:46:23 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,8 +1,9 @@
 import boto3
+import pandas as pd
 
-def run_athena_query(query, database, s3_output):
+def run_athena_query_and_get_results(query, database, s3_output):
     """
-    Executa uma consulta no Athena e retorna o resultado.
+    Executa uma consulta no Athena e retorna os resultados em um dataframe do pandas.
     
     Parâmetros:
     - query: A consulta SQL que você deseja executar.
@@ -29,18 +30,24 @@ def run_athena_query(query, database, s3_output):
         response = client.get_query_execution(QueryExecutionId=query_execution_id)
         status = response['QueryExecution']['Status']['State']
     
-    if status == 'SUCCEEDED':
-        print("Consulta concluída com sucesso.")
-    elif status == 'FAILED':
-        print("Consulta falhou.")
-    else:
-        print("Consulta foi cancelada.")
+    if status != 'SUCCEEDED':
+        print("Consulta não teve sucesso.")
+        return None
     
-    return status
+    # Se a consulta foi bem-sucedida, leia os resultados do S3 em um dataframe do pandas
+    result_s3_path = s3_output + query_execution_id + '.csv'
+    s3 = boto3.resource('s3')
+    bucket_name, key_name = result_s3_path.replace("s3://", "").split("/", 1)
+    obj = s3.Object(bucket_name, key_name)
+    data = obj.get()['Body'].read().decode('utf-8')
+    
+    dataframe = pd.read_csv(pd.compat.StringIO(data))
+    
+    return dataframe
 
 # Exemplo de uso:
 query = "SELECT * FROM minha_tabela LIMIT 10;"
 database = "meu_banco_de_dados"
 s3_output = "s3://meu-bucket/caminho/para/saida/"
-run_athena_query(query, database, s3_output)
-``
+df = run_athena_query_and_get_results(query, database, s3_output)
+print(df)
