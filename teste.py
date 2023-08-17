a94dbba13d2d77345a0a03fe860ba1957f80926b
Iago Victor <55510607+iiagovictor@users.noreply.github.com>
2023-08-17 09:11:21 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,5 @@
-', '.join([f"'{item}'" for item in lista])
+SELECT *
+FROM default.system.query_history
+WHERE date_diff('day', from_iso8601_timestamp(query_execution_time), current_timestamp) <= 30
+AND workgroup = 'NOME_DO_SEU_WORKGROUP'
+ORDER BY query_execution_time DESC;
