d8025612e6bef0976e23010154ca9125dee5bc20
Iago Victor <55510607+iiagovictor@users.noreply.github.com>
2023-08-22 11:52:54 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,1 @@
-pattern = re.compile(r'(?i)(?:from|join|union)[\s\n]+([a-zA-Z0-9_]+)\.[\s\n]*([a-zA-Z0-9_]+)')
+pattern = re.compile(r'(?i)(?:from|join|union)[\s\n]+\"([a-zA-Z0-9_]+)\"\.\"([\s\n]*[a-zA-Z0-9_]+)\"')
