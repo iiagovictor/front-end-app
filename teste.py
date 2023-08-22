2544302eb622ae13b2902d9206b814ea8c0f2340
Iago Victor <55510607+iiagovictor@users.noreply.github.com>
2023-08-22 11:09:37 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,38 +1,74 @@
+import boto3
 import re
+import json
+from datetime import datetime, timedelta
 
-def validate_resource_name(key):
-    key = key.lower()
+def get_tables_from_queries(workgroups):
+    # Criar um cliente para o Athena
+    client = boto3.client('athena')
+    
+    # Definir a data de início e fim para buscar as queries do dia anterior
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=1)
+    
+    # Conjunto para armazenar os nomes das tabelas de forma única
+    tables_set = set()
+    
+    # Expressão regular para encontrar tabelas
+    pattern = re.compile(r'(?i)(?:from|join)\s+([a-zA-Z0-9_]+)\.([a-zA-Z0-9_]+)')
+    
+    # Iterar sobre cada workgroup
+    for workgroup in workgroups:
+        next_token = None
+        while True:
+            # Obter as execuções de query
+            if next_token:
+                response = client.list_query_executions(
+                    WorkGroup=workgroup,
+                    MaxResults=1000,
+                    NextToken=next_token
+                )
+            else:
+                response = client.list_query_executions(
+                    WorkGroup=workgroup,
+                    MaxResults=1000
+                )
+            
+            # Se não houver mais execuções de query, sair do loop
+            if 'QueryExecutionIds' not in response:
+                break
+            
+            # Iterar sobre cada ID de execução de query
+            for query_id in response['QueryExecutionIds']:
+                # Obter os detalhes da execução da query
+                query_details = client.get_query_execution(
+                    QueryExecutionId=query_id
+                )
+                
+                # Verificar a data da execução da query
+                query_date = query_details['QueryExecution']['Status']['SubmissionDateTime']
+                if query_date < start_date:
+                    return [json.loads(item) for item in tables_set]
+                
+                # Extrair a query SQL
+                sql = query_details['QueryExecution']['Query']
+                
+                # Usar a expressão regular para encontrar os nomes das tabelas
+                matches = pattern.findall(sql)
+                for match in matches:
+                    table_dict = {'database': match[0], 'table': match[1]}
+                    tables_set.add(json.dumps(table_dict))
+            
+            # Se houver um token para a próxima página, atualize o next_token
+            next_token = response.get('NextToken')
+            if not next_token:
+                break
+    
+    # Converter o conjunto para uma lista de dicionários
+    tables = [json.loads(item) for item in tables_set]
+    
+    return tables
 
-    if not re.match(r'^(run|temp|proj)_', key):
-        return False, 'delete', 'Não especificado o tipo do projeto (RUN/PROJ/TEMP)'
-
-    if not re.match(r'^(run|temp|proj)_[a-z]{2}\d{4}_', key):
-        return False, 'delete', 'LUP inválido'
-
-    if not re.match(r'^(run)_', key) and not re.match(r'^(temp|proj)_[a-z]{2}\d{4}_[a-z]{7}_', key):
-        return False, 'delete', 'RACF inválido'
-
-    match = re.match(r'^(run|temp|proj)_[a-z]{2}\d{4}_(?:[a-z]{7}_)?([\w\s]+)$', key)
-    if match:
-        project_name = match.group(2)
-        if len(project_name) >= 1:
-            return True, 'keep', ''
-        else:
-            return False, 'delete', 'Nome do recurso deve ter 1 caracter no mínimo'
-    else:
-        return False, 'delete', 'Formato inválido'
-
-# Teste
-keys = [
-    "run_ab1234_project name",
-    "temp_ab1234_username_project name with spaces",
-    "proj_ab1234_project-name",
-    "proj_ab1234_username_project name"
-]
-
-for s in keys:
-    valid, action, reason = validate_resource_name(s)
-    if valid:
-        print(f"'{s}' é válido")
-    else:
-        print(f"'{s}' é inválido - {reason}")
+# Testar a função
+workgroups = ['workgroup1', 'workgroup2']
+print(get_tables_from_queries(workgroups))
