fb2e85eaf021d44f2305fe071a34bdb8bff02e19
Iago Victor <55510607+iiagovictor@users.noreply.github.com>
2023-10-11 11:46:45 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,22 @@
-aws eks --region <sua-regiao> update-kubeconfig --name <nome-do-cluster-EKS>
+import boto3
+
+# Nome da função Lambda e o nome da variável de ambiente a ser atualizada
+function_name = 'sua-funcao-lambda'
+variable_name = 'NOME_DA_VARIAVEL_AMBIENTE'
+new_value = 'NOVO_VALOR'
+
+# Inicialize o cliente do AWS Lambda
+lambda_client = boto3.client('lambda')
+
+# Recupere a configuração atual da função Lambda
+response = lambda_client.get_function_configuration(FunctionName=function_name)
+current_environment = response['Environment']['Variables']
+
+# Atualize a variável de ambiente com o novo valor
+current_environment[variable_name] = new_value
+
+# Atualize a configuração da função Lambda com a nova variável de ambiente
+lambda_client.update_function_configuration(
+    FunctionName=function_name,
+    Environment={'Variables': current_environment}
+)
